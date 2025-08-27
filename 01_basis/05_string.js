02e0e7f235e3abf93ad0415b69d85741661be273
Vikas Tiwari <vikastiwariv909@gmail.com>
2025-08-27 19:55:50 +0000

    string.js

---
+++ 01_basis/05_string.js
@@ -0,0 +1,29 @@
+const name = "John";
+const replace = 50;
+
+console.log(`My name is ${name} and I am ${replace} years old.`);
+
+const gameName = new String("Cricket"); 
+console.log(`My favorite game is ${gameName}.`);
+
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf("t"));
+
+
+const anotherGame = new String("Football");
+console.log(`My another favorite game is ${anotherGame}.`);
+
+console.log(anotherGame.charAt(2));
+console.log(anotherGame.indexOf("t"));
+
+const newString = gameName.substring(0, 4);
+console.log(`New string is: ${newString}`);
+
+const url = "https://www.example.com/path/to/page?name=John&age=30";
+
+console.log(url.replace(`John`, `Doe`)); // jo replace karna ho o phale likho aur jisse karna ho o baad me 
+
+console.log(url.includes(`vikas`));
+console.log(url.includes(`vikas`));
+console.log(url.includes(`vikas`));
+console.log(url.includes(`vikas`));
